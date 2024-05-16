65ff53f42219f0643b962e55a38a57a6cd1b645b
VDobryvechir <vdobryvechir@gmail.com>
2024-05-16 13:22:00 +0200

    config for building

--- build/ci/buildMainWin.cmd
+++ build/ci/buildMainWin.cmd
@@ -1,4 +1,4 @@
 @del %GOPATH%\bin\tvengine.exe
-@Xcopy /E /I /Y C:\cloud\microcore\pkg %GOPATH%\pkg\mod\github.com\!dobryvechir\microcore@v1.0.0\pkg 
+@Xcopy /E /I /Y C:\prg\libgo\microcore\pkg %GOPATH%\pkg\mod\github.com\!dobryvechir\microcore@v1.0.0\pkg 
 go install github.com/VDobryvechir/tvengine/pkg/main
 @ren %GOPATH%\bin\main.exe tvengine.exe
