75be35eb875e6f3dab59a24a4fce6f6c9170fe24
VDobryvechir <vdobryvechir@gmail.com>
2024-06-07 15:22:54 +0200

    landing page

--- build/ci/buildMainLinux.cmd
+++ build/ci/buildMainLinux.cmd
@@ -1,11 +1,7 @@
-Xcopy /E /I /Y F:\cloud\go\root\src\github.com\Dobryvechir\microcore\pkg C:\Users\volod\go\pkg\mod\github.com\!dobryvechir\microcore@v1.0.0\pkg 
-Xcopy /E /I /Y F:\cloud\go\root\src\github.com\VDobryvechir\dvserver\pkg ..\..\pkg 
-copy ..\..\test\main.go ..\..\pkg\main\main.go
 del %GOPATH%\bin\linux_amd64\main
-call copyAllConfigs.cmd
 SET CGO_ENABLED=0
 SET GOOS=linux
 go install github.com/VDobryvechir/tvengine/pkg/main
-@copy %GOPATH%\bin\linux_amd64\main F:\a\nec\order-capture-config\configs\oeconf\oeconf 
+@copy %GOPATH%\bin\linux_amd64\main c:\prg\tvengine\tvengine 
 @del %GOPATH%\bin\linux_amd64\main
 
--- build/ci/compileMainWin.cmd
+++ build/ci/compileMainWin.cmd
@@ -1,4 +1,5 @@
 @del %GOPATH%\bin\tvengine.exe
+@Xcopy /E /I /Y C:\prg\libgo\microcore\pkg %GOPATH%\pkg\mod\github.com\!dobryvechir\microcore@v1.0.2\pkg 
 go install github.com/VDobryvechir/tvengine/pkg/main
 @ren %GOPATH%\bin\main.exe tvengine.exe
 copy %GOPATH%\bin\tvengine.exe c:\prg\tvengine\tvengine.exe
